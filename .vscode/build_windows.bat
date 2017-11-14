fd3cce84fc1ea23c4ebfb2e202c027b9223499cd
Maxwell Mowbray <mmowbray@users.noreply.github.com>
2017-11-13 21:40:46 -0500

    Rename /libs to /lib

--- .vscode/build_windows.bat
+++ .vscode/build_windows.bat
@@ -1,12 +1,12 @@
 @echo off
 
 if not exist "build" mkdir "build"
-xcopy /s /Y libs build
+xcopy /s /Y lib build
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 set compilerflags=/Od /Zi /EHsc /Fo.\build\
 set sourcefiles=src/cpp/*.cpp
 set linkerflags=opengl32.lib glew32.lib glfw3dll.lib /OUT:build/opengl-app.exe
 
-cl.exe %compilerflags% %sourcefiles% /I include /link %linkerflags% /LIBPATH:libs
+cl.exe %compilerflags% %sourcefiles% /I include /link %linkerflags% /LIBPATH:libf
 
