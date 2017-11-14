956eae0e02bba7eb6340873913f44a135ae8700c
Maxwell Mowbray <mmowbray@users.noreply.github.com>
2017-11-13 21:06:58 -0500

    Add build_window.bat file

---
+++ .vscode/build_windows.bat
@@ -0,0 +1,12 @@
+@echo off
+
+if not exist "build" mkdir "build"
+xcopy /s /Y libs build
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+set compilerflags=/Od /Zi /EHsc /Fo.\build\
+set sourcefiles=src/cpp/*.cpp
+set linkerflags=opengl32.lib glew32.lib glfw3dll.lib /OUT:build/opengl-app.exe
+
+cl.exe %compilerflags% %sourcefiles% /I include /link %linkerflags% /LIBPATH:libs
+
